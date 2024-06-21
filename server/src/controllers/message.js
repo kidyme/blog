ccb4a6431a3a0cc4676eb79af1f9c75cb28c8493
kidyme <1554768198@qq.com>
2024-06-21 16:55:14 +0800

    modify Message删除接口修改

--- server/src/controllers/message.js
+++ server/src/controllers/message.js
@@ -1,10 +1,13 @@
 import Message from "../models/message.js";
 import { buildRes, baseCRUD } from "../utils/base.js";
 
-export const { add, find, findAll, update } = baseCRUD(Message, "Message");
+export const { add, find, findAll, update, remove } = baseCRUD(
+  Message,
+  "Message",
+);
 
-const remove = async (_) => {
-  return buildRes("fail", 200, "删除接口未实现");
-};
-
-export { remove };
+// const remove = async (_) => {
+//   return buildRes("fail", 200, "删除接口未实现");
+// };
+//
+// export { remove };
